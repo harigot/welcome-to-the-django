b704c2f7a9afc1e4c7d2e93ac33eed37018afef8
Harigot <ofbrunobm@gmail.com>
2022-04-01 08:58:42 -0300

    chore: readme fix

--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
 # Welcome to Hyperdrated
-### Never thirst again! with hyperhydrate you can see where the nearest public drinking fountain is!
+### Never get thirst again! with hyperhydrate you can see where the nearest public drinking fountain is!
 
--- usersys/urls.py
+++ usersys/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    #path('', views.home, name='home'),
     path('login/', views.signin, name='signin'),
     path('register/', views.signup, name='signup'),
     path('signout/', views.signout, name='signout'),
