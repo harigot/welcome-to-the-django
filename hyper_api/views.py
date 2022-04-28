b4856994351dcce29dc9e8df14df84f96d1b9418
Harigot <ofbrunobm@gmail.com>
2022-04-27 22:45:24 -0300

    feat: vote sistem

--- hyper_api/views.py
+++ hyper_api/views.py
@@ -1,9 +1,6 @@
 from .serializers import UserSerializer
-from rest_framework.exceptions import NotFound
-from rest_framework.response import Response
 from django.contrib.auth.models import User
 from rest_framework import viewsets
-from rest_framework import status
 
 
 
